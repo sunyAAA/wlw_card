246ef5b8b999d0ad895e0eb0aa7dcf7295973151
SunyAAA <xsannie15@gmail.com>
2018-08-13 15:41:51 +0800

    add waves

--- src/pages/viewBox/viewBox.vue
+++ src/pages/viewBox/viewBox.vue
@@ -1,22 +1,17 @@
  <template>
     <div class="view-box">
         <div class="slide-bar" :style="barStyle">
-             <p class="gs-title">{{title}}</p>
-             <el-menu
-                :default-active="defaultIndex"
-                background-color="#545c64"
-                text-color="#fff"
-                
-                    @select='select'>
-                <el-menu-item index="0">
+            <p class="gs-title">{{title}}</p>
+            <el-menu :default-active="defaultIndex" background-color="#545c64" text-color="#fff" @select='select'>
+                <el-menu-item index="0" v-waves>
                     <i class="el-icon-search"></i>
-                        <span slot="title">统计</span>
+                    <span slot="title">统计</span>
                 </el-menu-item>
-                <el-menu-item index="1">
+                <el-menu-item index="1" v-waves>
                     <i class="el-icon-menu"></i>
                     <span slot="title">流量池</span>
                 </el-menu-item>
-                <el-menu-item index="2">
+                <el-menu-item index="2" v-waves>
                     <i class="el-icon-setting"></i>
                     <span slot="title">设备</span>
                 </el-menu-item>
@@ -24,46 +19,48 @@
         </div>
         <div class="content">
             <top-bar @out='sendOut'></top-bar>
-            <router-view ></router-view>
+            <router-view></router-view>
         </div>
     </div>
 </template>
 
 <script>
 import TopBar from "../../components/topBar/topBar";
+import Waves from "../../directives/waves/waves.js";
 import storage from "good-storage";
 export default {
-  data() {
-    return {
-      routeList: ["/total", "/pool", "/device"],
-      defaultIndex: "0",
-      title: "物联网",
-      barStyle: {
-        width: "240px"
-      }
-    };
-  },
-  methods: {
-    select(index) {
-      if (!storage.get("u")) {
-        this.$notify({
-          title: "警告",
-          message: "登录已失效，请重新登录",
-          offset: 200
-        });
-        this.$emit("fail");
-      }
-      this.$router.push({ path: this.routeList[index] });
+    directives: { Waves },
+    data() {
+        return {
+            routeList: ["/total", "/pool", "/device"],
+            defaultIndex: "0",
+            title: "物联网",
+            barStyle: {
+                width: "240px"
+            }
+        };
     },
-    sendOut() {
-      this.$emit("out");
-    }
-  },
-  created() {
-    let i = this.routeList.indexOf(this.$route.path);
-    this.defaultIndex = (i < 0 ? 0 : i).toString();
-  },
-  components: { TopBar }
+    methods: {
+        select(index) {
+            if (!storage.get("u")) {
+                this.$notify({
+                    title: "警告",
+                    message: "登录已失效，请重新登录",
+                    offset: 200
+                });
+                this.$emit("fail");
+            }
+            this.$router.push({ path: this.routeList[index] });
+        },
+        sendOut() {
+            this.$emit("out");
+        }
+    },
+    created() {
+        let i = this.routeList.indexOf(this.$route.path);
+        this.defaultIndex = (i < 0 ? 0 : i).toString();
+    },
+    components: { TopBar }
 };
 </script>
 
